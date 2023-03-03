bf9622dac7522e9ab276855cf41904bba39aad92
Laurence <laurence.jones@live.co.uk>
2023-03-03 12:34:07 +0000

    Update functions to middlewares and final format is also http handler

--- formatters.go
+++ formatters.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"sort"
 	"strings"
 
 	"github.com/crowdsecurity/crowdsec/pkg/models"
 )
 
-var FormattersByName map[string]func([]*models.Decision) string = map[string]func([]*models.Decision) string{
+var FormattersByName map[string]func(w http.ResponseWriter, r *http.Request) = map[string]func(w http.ResponseWriter, r *http.Request){
 	"plain_text": PlainTextFormatter,
 	"microtik":   MicroTikFormatter,
 }
 
-func PlainTextFormatter(decisions []*models.Decision) string {
+func PlainTextFormatter(w http.ResponseWriter, r *http.Request) {
+	decisions := r.Context().Value("decisions").([]*models.Decision)
 	ips := make([]string, len(decisions))
 	for i, decision := range decisions {
 		ips[i] = *decision.Value
 	}
 	sort.Strings(ips)
-	return strings.Join(ips, "\n")
+	w.Write([]byte(strings.Join(ips, "\n")))
 }
 
-func MicroTikFormatter(decisions []*models.Decision) string {
+func MicroTikFormatter(w http.ResponseWriter, r *http.Request) {
+	decisions := r.Context().Value("decisions").([]*models.Decision)
 	ips := make([]string, len(decisions))
+	listName := r.URL.Query().Get("listname")
+	if listName == "" {
+		listName = "CrowdSec"
+	}
 	for i, decision := range decisions {
 		var ipType = "/ip"
 		if strings.Contains(*decision.Value, ":") {
@@ -38,7 +45,7 @@ func MicroTikFormatter(decisions []*models.Decision) string {
 		)
 	}
 	sort.Strings(ips)
-	return "/ip firewall address-list remove [find list=CrowdSec]\n" +
-		"/ipv6 firewall address-list remove [find list=CrowdSec]\n" +
-		strings.Join(ips, "\n")
+	w.Write([]byte(fmt.Sprintf("/ip firewall address-list remove [find list=%s]\n", listName) +
+		fmt.Sprintf("/ipv6 firewall address-list remove [find list=%s]\n", listName) +
+		strings.Join(ips, "\n")))
 }
