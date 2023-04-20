e945dd94331a0d70caa92342afa7323a2a739183
Marco Mariani <marco@crowdsec.net>
2023-04-20 12:28:14 +0200

    update test

--- test/test_scripts_no_crowdsec.py
+++ test/test_scripts_no_crowdsec.py
@@ -1,5 +1,6 @@
 import os
 import pexpect
+import yaml
 
 import pytest
 
@@ -24,10 +25,22 @@ def test_install_no_crowdsec(project_repo, bouncer_binary, must_be_root):
     assert c.terminated
     assert c.exitstatus == 0
 
+    with open(CONFIG) as f:
+        y = yaml.safe_load(f)
+        assert y['crowdsec_config']['lapi_key'] == '<API_KEY>'
+        assert y['crowdsec_config']['lapi_url'] == '${CROWDSEC_LAPI_URL}'    # XXX: we may want to change this
+
     assert os.path.exists(CONFIG)
     assert os.stat(CONFIG).st_mode & 0o777 == 0o600
     assert os.path.exists(f'/usr/local/bin/{BOUNCER}')
     assert os.stat(f'/usr/local/bin/{BOUNCER}').st_mode & 0o777 == 0o755
+
+    c = pexpect.spawn(
+        '/usr/bin/sh', ['scripts/install.sh'],
+        cwd=project_repo
+    )
+
+    c.expect(f"WARN.* /usr/local/bin/{BOUNCER} is already installed. Exiting")
 
 
 @pytest.mark.dependency(depends=['test_install_no_crowdsec'])
