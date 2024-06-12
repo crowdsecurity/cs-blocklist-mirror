e3f8aef0a27deb8542837bbff5cbbf3b7aa5b9d0
j3n57h0m45 <77627854+j3n57h0m45@users.noreply.github.com>
2024-06-12 10:41:34 +0000

    refactor mikrotik formatter

--- pkg/formatters/formatters.go
+++ pkg/formatters/formatters.go
@@ -7,12 +7,13 @@ import (
 
 	"github.com/crowdsecurity/crowdsec/pkg/models"
 
+	"github.com/crowdsecurity/cs-blocklist-mirror/pkg/formatters/mikrotik"
 	"github.com/crowdsecurity/cs-blocklist-mirror/pkg/registry"
 )
 
 var ByName = map[string]func(w http.ResponseWriter, r *http.Request){
 	"plain_text": PlainText,
-	"mikrotik":   MikroTik,
+	"mikrotik":   mikrotik.Format,
 	"f5":         F5,
 }
 
@@ -20,39 +21,6 @@ func PlainText(w http.ResponseWriter, r *http.Request) {
 	decisions := r.Context().Value(registry.GlobalDecisionRegistry.Key).([]*models.Decision)
 	for _, decision := range decisions {
 		fmt.Fprintf(w, "%s\n", *decision.Value)
-	}
-}
-
-func MikroTik(w http.ResponseWriter, r *http.Request) {
-	decisions := r.Context().Value(registry.GlobalDecisionRegistry.Key).([]*models.Decision)
-
-	listName := r.URL.Query().Get("listname")
-	if listName == "" {
-		listName = "CrowdSec"
-	}
-
-	if !r.URL.Query().Has("ipv6only") {
-		fmt.Fprintf(w, "/ip firewall address-list remove [find list=%s]\n", listName)
-	}
-
-	if !r.URL.Query().Has("ipv4only") {
-		fmt.Fprintf(w, "/ipv6 firewall address-list remove [find list=%s]\n", listName)
-	}
-
-	for _, decision := range decisions {
-		var ipType = "/ip"
-		if strings.Contains(*decision.Value, ":") {
-			ipType = "/ipv6"
-		}
-
-		fmt.Fprintf(w,
-			"%s firewall address-list add list=%s address=%s comment=\"%s for %s\"\n",
-			ipType,
-			listName,
-			*decision.Value,
-			*decision.Scenario,
-			*decision.Duration,
-		)
 	}
 }
 
---
+++ pkg/formatters/mikrotik/mikrotik.go
@@ -0,0 +1,106 @@
+package mikrotik
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/crowdsecurity/crowdsec/pkg/models"
+	"github.com/crowdsecurity/cs-blocklist-mirror/pkg/registry"
+)
+
+type CustomFirewallDecisionData struct {
+	IpAddress string
+	Scenario  string
+	Duration  string
+	IsIPv6    bool
+}
+
+type CustomMikrotikData struct {
+	ListName               string
+	Decisions              []CustomFirewallDecisionData
+	NameOfMikrotikFunction string
+	IPv6Only               bool
+	IPv4Only               bool
+}
+
+//go:embed mikrotik.tmpl
+var MikrotikScriptTemplateFile embed.FS
+
+func Format(w http.ResponseWriter, r *http.Request) {
+
+	// Extract decisions from the context
+	decisions := r.Context().Value(registry.GlobalDecisionRegistry.Key).([]*models.Decision)
+
+	// Get query parameters
+	query := r.URL.Query()
+
+	// check if ipv6only or ipv4only is set
+	ipv6only := query.Has("ipv6only")
+	ipv4only := query.Has("ipv4only")
+
+	listName := query.Get("listname")
+	if listName == "" {
+		listName = "CrowdSec"
+	}
+	// Prepare data for the template
+	var decisionData []CustomFirewallDecisionData
+	for _, decision := range decisions {
+		decisionData = append(decisionData, CustomFirewallDecisionData{
+			IpAddress: *decision.Value,
+			Scenario:  *decision.Scenario,
+			Duration:  *decision.Duration,
+			IsIPv6:    strings.Contains(*decision.Value, ":"),
+		})
+	}
+
+	data := CustomMikrotikData{
+		ListName:               listName,
+		Decisions:              decisionData,
+		NameOfMikrotikFunction: "CrowdSecBlockIP",
+		IPv6Only:               ipv6only,
+		IPv4Only:               ipv4only,
+	}
+
+	MikrotikScriptTemplate, err := fs.ReadFile(MikrotikScriptTemplateFile, "mikrotik.tmpl")
+	if err != nil {
+		http.Error(w, "Error reading template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Parse the template
+	parsedTemplate, err := template.New("script").Parse(string(MikrotikScriptTemplate))
+	if err != nil {
+		http.Error(w, "Error parsing template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template
+	var script strings.Builder
+	err = parsedTemplate.Execute(&script, data)
+	if err != nil {
+		http.Error(w, "Error executing template "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Remove empty lines and trim leading/trailing white spaces from the script
+	scriptString := strings.TrimSpace(strings.ReplaceAll(script.String(), "\n\n", "\n"))
+	script.Reset()
+	script.WriteString(scriptString)
+
+	// Get the content length of the script
+	contentLength := len([]byte(script.String()))
+
+	// Set the Content-Length header
+	w.Header().Set("Content-Length", strconv.Itoa(contentLength))
+
+	// Write the script to the http.ResponseWriter
+	_, err = w.Write([]byte(script.String()))
+	if err != nil {
+		w.Header().Del("Content-Length")
+		http.Error(w, "Error writing response "+err.Error(), http.StatusInternalServerError)
+	}
+}
